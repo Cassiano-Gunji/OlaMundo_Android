b7404d435c2c761ed3b13a41da3207a24cf8c49c
Cassiano Gunji <Cassiano Gunji>
2016-10-22 20:10:54 -0200

    Initial commit

---
+++ app/src/main/java/br/unip/olamundo/OlaMundo.java
@@ -0,0 +1,13 @@
+package br.unip.olamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OlaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ola_mundo);
+    }
+}
